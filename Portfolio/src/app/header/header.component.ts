4344855670ea23f102e8cf1b2ce569356378439f
RAVI <rishwanth.ravi@accenture.com>
2023-08-07 17:03:47 +0530

    Changes for skill component

--- Portfolio/src/app/header/header.component.ts
+++ Portfolio/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Inject  } from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
+import { HtmlParser } from '@angular/compiler';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(@Inject (DOCUMENT) document: Document){
+    document.getElementById('el');
+  }
 
   menuClick : boolean = true;
+  
+  
   OnMenuClick(){
     this.menuClick = !this.menuClick;
+    if(this.menuClick == true){
+      
+      const navbar = document.getElementById("Menu")  
+      console.log(navbar?.style);      
+      if(navbar?.style["display"] == "block"){
+          console.log("its none");          
+      }
+    }
+    
   }
 
 }
