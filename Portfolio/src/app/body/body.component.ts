5ef4a750682cd21d05094ac7349af27935072456
RAVI <rishwanth.ravi@accenture.com>
2023-08-09 10:58:58 +0530

    changes for role in body

--- Portfolio/src/app/body/body.component.ts
+++ Portfolio/src/app/body/body.component.ts
@@ -9,4 +9,5 @@ export class BodyComponent {
 
     details : string[] = ["../../../assets/SKILLS/html.png","../../../assets/SKILLS/css-3.png","../../../assets/SKILLS/python.png","../../../assets/SKILLS/.NEt.png","../../../assets/SKILLS/C_sharp.png","../../../assets/SKILLS/github.png",
                           "../../../assets/SKILLS/js.png","../../../assets/SKILLS/postgres.png","../../../assets/SKILLS/netcore.png","../../../assets/SKILLS/Angular.png"]
-}
+
+    }
