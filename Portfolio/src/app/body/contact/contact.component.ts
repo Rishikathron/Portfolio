b3d973e140720e31a9d9a32b239980807a23df22
Rishwanth Ravi <rishwanth.ravi@accenture.com>
2024-09-06 12:19:06 +0530

    contact changes

--- Portfolio/src/app/body/contact/contact.component.ts
+++ Portfolio/src/app/body/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  phnumber : any = '';
+  name : any = '';
+  subject : any = '';
+  message : any = '';
+  email : any = '';
+ 
+
+  onsendmessage(){
+   
+    setTimeout( function ( ) { alert('Email Sent! Thanks for reaching me out'); }, 1000 ); 
+    setTimeout( 
+      function ( ) {         
+        alert('For any immediate queries, feel free to call me — I promise I don\'t bite (unless you\'re a spam call, then all bets are off)! 😊📞'); 
+      }, 1200 );
+      this.phnumber = '';
+      this.name  = '';
+      this.subject  = '';
+      this.message  = '';
+      this.email = '';
+  }
 }
